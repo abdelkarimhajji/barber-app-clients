9b4c2ecea98d2635a7350c2642a00bc08f6ab3fa
abdelkarimhajji <abdelkarim.hujji@gmail.com>
2024-06-28 05:30:53 +0100

    i fixed some responsive problems in availabel barbers

--- src/components/AvailabelBarbers.js
+++ src/components/AvailabelBarbers.js
@@ -6,13 +6,13 @@ import { IoIosPersonAdd } from "react-icons/io";
 
 function AvailabelBarbers(){
     return (
-        <div className="w-full px-5 480:px-5">
+        <div className="w-full px-5 ">
             <div className="pl-3"><p className="font-extrabold text-xl text-green-600 mb-6">Barbers Availabel</p></div>
-            <div className=" w-full 400:grid grid-cols-2  md:grid-cols-3  gap-x-4 480:gap-x-2 gap-y-4  480:gap-y-12">
-                <div className="  w-[100%] 400:w-[100%]  sm:h-[350px] h-[320px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center">
-                    <div className="w-full h-[45%] rounded-t-[20px] relative z-0">
+            <div className=" w-full 400:grid grid-cols-2  md:grid-cols-3  gap-x-4 480:gap-x-2   400:gap-y-3">
+                <div className="  w-[100%] 400:w-[100%]  sm:h-[320px] h-[320px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center mb-[20px] 400:mb-0">
+                    <div className="w-full h-[45%] rounded-t-[20px] relative z-0 overflow-hidden">
                         <img src={beard} alt="" srcset="" className="w-full h-full object-cover rounded-t-[20px]"/>
-                        {/* <div className='absolute top-5 left-4 rounded-xl w-[50px] h-[20px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div> */}
+                        {/* <div className='absolute top-5 left-4 rounded-xl py-[0.6px] px-[7px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div> */}
                         <div className='absolute top-5 left-4 rounded-xl w-[55px] h-[20px] bg-red-800 text-[12px] text-white font-bold flex items-center justify-center'>Closed</div>
                     </div>
                     <div className=' w-full text-white pt-4 px-4'>
@@ -25,13 +25,13 @@ function AvailabelBarbers(){
                         <p className={`text-white ml-6  flex items-center flex-shrink-0`}><MdRateReview className='text-green-600 480:text-[15px]'/><span className='text-[12px] ml-1'>1,500</span></p>
                     </div>  
                     <div className='w-[100%]   flex justify-center items-end'>
-                        <button type="button" className='text-white text-sm bg-green-700 py-2 sm:px-10 w-[80%] sm:w-0 rounded-xl'>Make Appointment</button>
+                        <button type="button" className='text-white text-sm bg-green-700 py-2 mb-3 overflow-hidden sm:px-10 w-[80%] sm:w-auto rounded-xl'>Make Appointment</button>
                     </div>
                 </div>
-                <div className="  w-[100%] 400:w-[100%]  sm:h-[350px] h-[320px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center">
-                    <div className="w-full h-[45%] rounded-t-[20px] relative z-0">
+                <div className="  w-[100%] 400:w-[100%]  h-[320px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center mb-[20px]  400:mb-0">
+                    <div className="w-full h-[45%] rounded-t-[20px] relative z-0 overflow-hidden">
                         <img src={beard} alt="" srcset="" className="w-full h-full object-cover rounded-t-[20px]"/>
-                        <div className='absolute top-5 left-4 rounded-xl w-[50px] h-[20px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div>
+                        <div className='absolute top-5 left-4 rounded-xl py-[0.6px] px-[7px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div>
                         {/* <div className='absolute top-5 left-4 rounded-xl w-[55px] h-[20px] bg-red-800 text-[12px] text-white font-bold flex items-center justify-center'>Closed</div> */}
                     </div>
                     <div className=' w-full text-white pt-4 px-4'>
@@ -44,14 +44,14 @@ function AvailabelBarbers(){
                         <p className={`text-white ml-6  flex items-center flex-shrink-0`}><MdRateReview className='text-green-600 480:text-[15px]'/><span className='text-[12px] ml-1'>1,500</span></p>
                     </div>  
                     <div className='w-[100%]   flex justify-center items-end'>
-                        <button type="button" className='text-white text-sm bg-green-700 py-2 sm:px-10 w-[80%] sm:w-0 rounded-xl'>Make Appointment</button>
+                        <button type="button" className='text-white text-sm mb-3 overflow-hidden bg-green-700 py-2 sm:px-10 w-[80%] sm:w-auto rounded-xl'>Make Appointment</button>
                     </div>
                 </div>
-                <div className="  w-[100%] 400:w-[100%]  sm:h-[350px] h-[320px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center">
-                    <div className="w-full h-[45%] rounded-t-[20px] relative z-0">
+                <div className="  w-[100%] 400:w-[100%]  h-[320px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center mb-[20px]  400:mb-0">
+                    <div className="w-full h-[45%] rounded-t-[20px] relative z-0 overflow-hidden">
                         <img src={beard} alt="" srcset="" className="w-full h-full object-cover rounded-t-[20px]"/>
-                        {/* <div className='absolute top-5 left-4 rounded-xl w-[50px] h-[20px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div> */}
-                        <div className='absolute top-5 left-4 rounded-xl w-[55px] h-[20px] bg-red-800 text-[12px] text-white font-bold flex items-center justify-center'>Closed</div>
+                        {/* <div className='absolute top-5 left-4 rounded-xl py-[0.6px] px-[7px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div> */}
+                        <div className='absolute top-5 left-4 rounded-xl py-[0.6px] px-[7px] bg-red-800 text-[12px] text-white font-bold flex items-center justify-center'>Closed</div>
                     </div>
                     <div className=' w-full text-white pt-4 px-4'>
                         <p className={`font-bold ${styles.truncate}`}>Salon Dodo</p>
@@ -63,13 +63,13 @@ function AvailabelBarbers(){
                         <p className={`text-white ml-6  flex items-center flex-shrink-0`}><MdRateReview className='text-green-600 480:text-[15px]'/><span className='text-[12px] ml-1'>1,500</span></p>
                     </div>  
                     <div className='w-[100%]   flex justify-center items-end'>
-                        <button type="button" className='text-white text-sm bg-green-700 py-2 sm:px-10 w-[80%] sm:w-0 rounded-xl'>Make Appointment</button>
+                        <button type="button" className='text-white text-sm mb-3 overflow-hidden bg-green-700 py-2 sm:px-10 w-[80%] sm:w-auto rounded-xl'>Make Appointment</button>
                     </div>
                 </div>
-                <div className="  w-[100%] 400:w-[100%]  sm:h-[350px] h-[320px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center">
-                    <div className="w-full h-[45%] rounded-t-[20px] relative z-0">
+                <div className="  w-[100%] 400:w-[100%]  h-[320px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center mb-[20px]  400:mb-0">
+                    <div className="w-full h-[45%] rounded-t-[20px] relative z-0 overflow-hidden">
                         <img src={beard} alt="" srcset="" className="w-full h-full object-cover rounded-t-[20px]"/>
-                        <div className='absolute top-5 left-4 rounded-xl w-[50px] h-[20px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div>
+                        <div className='absolute top-5 left-4 rounded-xl py-[0.6px] px-[7px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div>
                         {/* <div className='absolute top-5 left-4 rounded-xl w-[55px] h-[20px] bg-red-800 text-[12px] text-white font-bold flex items-center justify-center'>Closed</div> */}
                     </div>
                     <div className=' w-full text-white pt-4 px-4'>
@@ -82,13 +82,13 @@ function AvailabelBarbers(){
                         <p className={`text-white ml-6  flex items-center flex-shrink-0`}><MdRateReview className='text-green-600 480:text-[15px]'/><span className='text-[12px] ml-1'>1,500</span></p>
                     </div>  
                     <div className='w-[100%]   flex justify-center items-end'>
-                        <button type="button" className='text-white text-sm bg-green-700 py-2 sm:px-10 w-[80%] sm:w-0 rounded-xl'>Make Appointment</button>
+                        <button type="button" className='text-white text-sm mb-3 overflow-hidden bg-green-700 py-2 sm:px-10 w-[80%] sm:w-auto rounded-xl'>Make Appointment</button>
                     </div>
                 </div>
-                <div className="  w-[100%] 400:w-[100%]  sm:h-[350px] h-[320px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center">
-                    <div className="w-full h-[45%] rounded-t-[20px] relative z-0">
+                <div className="  w-[100%] 400:w-[100%]  h-[320px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center mb-[20px]  400:mb-0">
+                    <div className="w-full h-[45%] rounded-t-[20px] relative z-0 overflow-hidden">
                         <img src={beard} alt="" srcset="" className="w-full h-full object-cover rounded-t-[20px]"/>
-                        {/* <div className='absolute top-5 left-4 rounded-xl w-[50px] h-[20px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div> */}
+                        {/* <div className='absolute top-5 left-4 rounded-xl py-[0.6px] px-[7px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div> */}
                         <div className='absolute top-5 left-4 rounded-xl w-[55px] h-[20px] bg-red-800 text-[12px] text-white font-bold flex items-center justify-center'>Closed</div>
                     </div>
                     <div className=' w-full text-white pt-4 px-4'>
@@ -101,7 +101,7 @@ function AvailabelBarbers(){
                         <p className={`text-white ml-6  flex items-center flex-shrink-0`}><MdRateReview className='text-green-600 480:text-[15px]'/><span className='text-[12px] ml-1'>1,500</span></p>
                     </div>  
                     <div className='w-[100%]   flex justify-center items-end'>
-                        <button type="button" className='text-white text-sm bg-green-700 py-2 sm:px-10 w-[80%] sm:w-0 rounded-xl'>Make Appointment</button>
+                        <button type="button" className='text-white text-sm mb-3 overflow-hidden bg-green-700 py-2 sm:px-10 w-[80%] sm:w-auto rounded-xl'>Make Appointment</button>
                     </div>
                 </div>
             </div>
