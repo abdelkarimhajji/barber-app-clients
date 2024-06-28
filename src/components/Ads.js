fbc4ab359600663053a16d56a7eadc0430d96e3e
abdelkarimhajji <abdelkarim.hujji@gmail.com>
2024-06-28 06:15:15 +0100

    i add buttons to by some think from ads side

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -1,20 +1,31 @@
 import styles from '../styles/App.module.css';
 import beard from '../imgs/beard.jpg';
-
+import { FaArrowRightLong } from "react-icons/fa6";
 function Ads()
 {
     return (
         <div className='px-5'>
             <div className={`w-full  mb-16  md:mb-20 overflow-y-scroll flex  text-white mt-[180px] 324:mt-[160px] 480:mt-[200px] gap-x-5 ${styles.hideScrollbar}`}>
-                
-                <div className="flex-shrink-0 shadow-black2">
+                <div className="flex-shrink-0 shadow-black2 relative">
                     <img src={beard} alt=""  className="object-cover rounded-[20px] w-[270px] h-[180px] sm:w-[500px] sm:h-[250px]"/>
+                    <div className='absolute flex justify-center items-center text-[12px] font-semibold top-[185px] left-[40px]  cursor-pointer rounded-xl px-[15px] py-[6px] bg-red-900 hover:bg-green-900 transition-all'>
+                        <p>By now </p>
+                        <FaArrowRightLong  className='ml-3' />
+                    </div>
                 </div>
-                <div className="flex-shrink-0 shadow-black2">
+                <div className="flex-shrink-0 shadow-black2 relative">
                     <img src={beard} alt=""  className="object-cover rounded-[20px] w-[270px] h-[180px] sm:w-[500px] sm:h-[250px]"/>
+                    <div className='absolute flex justify-center items-center text-[12px] font-semibold top-[185px] left-[40px]  cursor-pointer rounded-xl px-[15px] py-[6px] bg-red-900 hover:bg-green-900 transition-all'>
+                        <p>By now </p>
+                        <FaArrowRightLong  className='ml-3' />
+                    </div>
                 </div>
-                <div className="flex-shrink-0 shadow-black2">
+                <div className="flex-shrink-0 shadow-black2 relative">
                     <img src={beard} alt=""  className="object-cover rounded-[20px] w-[270px] h-[180px] sm:w-[500px] sm:h-[250px]"/>
+                    <div className='absolute flex justify-center items-center text-[12px] font-semibold top-[185px] left-[40px]  cursor-pointer rounded-xl px-[15px] py-[6px] bg-red-900 hover:bg-green-900 transition-all'>
+                        <p>By now </p>
+                        <FaArrowRightLong  className='ml-3' />
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -6,8 +6,8 @@ import { BiSolidHome } from "react-icons/bi";
 
 function BottomMenu() {
     return (
-        <div className="fixed bottom-0 w-full lg:w-[70%] 2xl:w-[1000px] sm:h-[100px]  h-[85px] flex justify-center items-start">
-            <div className="text-white w-[90%] sm:w-[450px] bg-customBlue shadow-black2 h-[70px] 375:px-[40px] px-[20px] rounded-[20px] flex items-center justify-between">
+        <div className="fixed bottom-0 w-full  lg:w-[70%] 2xl:w-[1000px] sm:h-[100px]  h-[85px]  flex justify-center items-start">
+            <div className="text-white  w-[90%] sm:w-[450px] bg-customBlue shadow-black2  h-[70px] 375:px-[40px] px-[20px] rounded-[20px] flex items-center justify-between">
                 <BiSolidHome className="sm:text-[29px] text-[25px] text-green-700 cursor-pointer"/>
                 <HiMenu  className="sm:text-[29px] text-[25px] cursor-pointer" />
                 <LuSearch  className="sm:text-[29px] text-[25px] cursor-pointer" />
