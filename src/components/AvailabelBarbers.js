f01b0b3af4b9999ad1f4d081f01a80d527d92409
abdelkarimhajji <abdelkarim.hujji@gmail.com>
2024-06-28 03:01:25 +0100

    i fixed style of commit and followers

--- src/components/AvailabelBarbers.js
+++ src/components/AvailabelBarbers.js
@@ -1,14 +1,15 @@
 import beard from '../imgs/beard.jpg';
 import styles from '../styles/App.module.css';
 import { FaHeart } from "react-icons/fa";
-import { LuMessagesSquare } from "react-icons/lu";
+import { MdRateReview } from "react-icons/md";
+import { IoIosPersonAdd } from "react-icons/io";
 
 function AvailabelBarbers(){
     return (
         <div className="w-full px-3 480:px-5">
             <div className="pl-3"><p className="font-extrabold text-xl text-green-600 mb-6">Barbers Availabel</p></div>
             <div className=" w-full grid grid-cols-2  md:grid-cols-3  gap-x-4 480:gap-x-2 gap-y-4  480:gap-y-12">
-                <div className="w-[100%] 480:w-[90%] sm:h-[350px] h-[300px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center">
+                <div className="w-[100%] 480:w-[90%] sm:h-[350px] min-h-[310px] shadow-black2 rounded-[20px]  mx-auto  flex flex-col items-center">
                     <div className="w-full h-[45%] rounded-t-[20px] relative z-0">
                         <img src={beard} alt="" srcset="" className="w-full h-full object-cover rounded-t-[20px]"/>
                         {/* <div className='absolute top-5 left-4 rounded-xl w-[50px] h-[20px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div> */}
@@ -18,9 +19,10 @@ function AvailabelBarbers(){
                         <p className={`font-bold ${styles.truncate}`}>Salon Dodo</p>
                         <p className={`text-gray text-sm ${styles.truncate}`}>abdelkarim hajji</p>
                     </div>
-                    <div className={`w-full  px-4 pt-4 flex overflow-hidden mb-4`}>
-                        <p className='flex items-center text-white'><FaHeart className='text-red-700'/> <span className='ml-1 text-[12px]'>20,200</span></p>
-                        <p className={`text-white ml-6 flex items-center `}><LuMessagesSquare  className='text-green-600'/><span className='text-[12px] ml-1'>1,500</span></p>
+                    <div className={`w-full  px-4 pt-4 flex flex-wrap overflow-hidden  mb-4`}>
+                        <p className='flex items-center text-white 586:mb-0 mb-2 md:mb-2'><FaHeart className='text-red-700 480:text-[15px]'/> <span className='ml-1 text-[12px]'>20,200</span></p>
+                        <p className={`text-white 375:ml-6 343:ml-2 ml-0 flex items-center 586:mb-0 mb-2 md:mb-2`}><IoIosPersonAdd className='text-green-600  480:text-[15px]'/><span className='text-[12px] ml-1'>1,500</span></p>
+                        <p className={`text-white 586:ml-6 w-[80px]  586:w-auto md:w-[100px] 940:w-auto md:ml-0 940:ml-6 940:mb-2 lg:ml-0 lg:w-[80px] 1219:w-auto 1219:ml-6   flex items-center `}><MdRateReview className='text-green-600 480:text-[15px]'/><span className='text-[12px] ml-1'>1,500</span></p>
                     </div>
                     <div>hi</div>
                 </div>
