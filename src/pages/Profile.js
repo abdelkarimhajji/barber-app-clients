9a495798cac8e529b71c79c2c6ade6c18b6186d3
abdelkarimhajji <abdelkarim.hujji@gmail.com>
2024-07-16 19:56:57 +0100

    i add to profile page window top

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,11 @@
-
+import {useEffect} from "react";    
 import NavBar from "../components/NavBar";
 import BottomMenu from "../components/BottomMenu";
 
 function Profile(){
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
     return (
         <div className="bg-login w-full  min-h-screen h-auto flex justify-center">
             <div className="bg-customBlue h-full min-h-screen w-full md:w-[100%] lg:w-[1000px] box-border relative ">
