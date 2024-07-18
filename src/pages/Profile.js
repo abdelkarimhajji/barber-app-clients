018bea8e2405e47c8ae30ae118998905383f28e6
abdelkarimhajji <abdelkarim.hujji@gmail.com>
2024-07-18 06:28:35 +0100

    finish style panel of info of profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,7 @@
 import {useEffect} from "react";    
 import NavBar from "../components/NavBar";
 import BottomMenu from "../components/BottomMenu";
-
+import { RiEditBoxFill } from "react-icons/ri";
 function Profile(){
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -11,8 +11,43 @@ function Profile(){
             <div className="bg-customBlue h-full min-h-screen w-full md:w-[100%] lg:w-[1000px] box-border relative ">
                 <NavBar profile="profile"/>
                 <div className="mt-[163px] 324:mt-[165px] 480:mt-[120px]"></div>
-                <div className="">
-                    ppp
+                <div className="w-full h-full px-[30px]">
+                    <div className="w-full bg-login h-[400px] overflow-hidden rounded-2xl relative text-white ">
+                        <div className="absolute w-[100px] h-[100px] bg-green-700 right-0 rounded-bl-[100px] flex items-center justify-center text-[35px] text-white"><RiEditBoxFill  className="ml-4 mt-[-13px] cursor-pointer hover:text-login transition-all duration-300"/></div>
+                        <div className="w-full px-[30px] pt-5 mb-[50px]">
+                            <p className="font-bold text-[20px]">Personal Info</p>
+                        </div>
+                        <div className="w-full h-full px-[100px] flex">
+                            <div className="  w-1/2">
+                                <div className="mb-[30px]">
+                                    <p className="font-bold text-xl mb-2">Full Name</p>
+                                    <p className="text-[16px] ml-1">Abdelkarim Hajji</p>
+                                </div>
+                                <div className="mb-[30px]">
+                                    <p className="font-bold text-xl mb-2">Gender</p>
+                                    <p className="text-[16px] ml-1">Male</p>
+                                </div>
+                                <div>
+                                    <p className="font-bold text-xl mb-2">E-mail</p>
+                                    <p className="text-[16px] ml-1">hajjikarim530@gmail.com</p>
+                                </div>
+                            </div>
+                            <div className=" w-1/2">
+                                <div className="mb-[30px]">
+                                    <p className="font-bold text-xl mb-2">Date of Birth</p>
+                                    <p className="text-[16px] ml-1">2001/04/20</p>
+                                </div>
+                                <div className="mb-[30px]">
+                                    <p className="font-bold text-xl mb-2">Phone</p>
+                                    <p className="text-[16px] ml-1">0617591321</p>
+                                </div>
+                                <div>
+                                    <p className="font-bold text-xl mb-2">Location</p>
+                                    <p className="text-[16px] ml-1">Tangier</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <BottomMenu location="profile"/>
             </div>
