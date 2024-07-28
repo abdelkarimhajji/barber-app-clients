0d96c9ae3b65a35daeb85822ec0137934c4eb308
abdelkarimhajji <abdelkarim.hujji@gmail.com>
2024-07-28 19:40:00 +0100

    i finish the responcive style

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -10,41 +10,41 @@ function Profile(){
         <div className="bg-login w-full  min-h-screen h-auto flex justify-center">
             <div className="bg-customBlue h-full min-h-screen w-full md:w-[100%] lg:w-[1000px] box-border relative ">
                 <NavBar profile="profile"/>
-                <div className="mt-[163px] 324:mt-[165px] 480:mt-[120px]"></div>
-                <div className="w-full h-full px-[30px]">
-                    <div className="w-full bg-login h-[400px] overflow-hidden rounded-2xl relative text-white ">
-                        <div className="absolute w-[80px] h-[80px] bg-green-700 right-0 rounded-bl-[100px] flex items-center justify-center text-[30px] text-white"><RiEditBoxFill  className="ml-4 mt-[-13px] cursor-pointer hover:text-login transition-all duration-300"/></div>
-                        <div className="w-full px-[30px] pt-5 mb-[50px]">
-                            <p className="font-bold text-[20px]">Personal Info</p>
+                <div className="mt-[163px] 324:mt-[110px] 480:mt-[120px]"></div>
+                <div className="w-full h-full 376:px-[30px] px-[10px]">
+                    <div className="w-full bg-login 570:h-[400px] overflow-hidden rounded-2xl relative text-white ">
+                        <div className="absolute 570:w-[80px] 570:h-[80px]  w-[50px] h-[50px] bg-green-700 right-0 rounded-bl-[100px] flex items-center justify-center 570:text-[30px] text-[20px] text-white"><RiEditBoxFill  className="570:ml-4 ml-2.5 570:mt-[-13px] mt-[-7px] cursor-pointer hover:text-login transition-all duration-300"/></div>
+                        <div className="w-full px-[30px] pt-5 500:mb-[50px] mb-[25px]">
+                            <p className="font-bold 500:text-[20px] text-[17px]">Personal Info</p>
                         </div>
-                        <div className="w-full h-full 600:px-[100px] px-[60px] flex">
+                        <div className="w-full h-full 600:px-[100px] 500:px-[60px]  pl-[30px] pr-[30px] flex mb-[30px]">
                             <div className="w-1/2">
                                 <div className="mb-[30px] ">
-                                    <p className="font-bold text-xl mb-2">Full Name</p>
-                                    <p className="text-[16px] ml-1">Abdelkarim Hajji</p>
+                                    <p className="font-bold 570:text-xl 500:text-md text-[13px]  mb-2">Full Name</p>
+                                    <p className="570:text-[16px] 500:text-[14px] text-[12px] ml-1">Abdelkarim Hajji</p>
                                 </div>
                                 <div className="mb-[30px]">
-                                    <p className="font-bold text-xl mb-2">Gender</p>
-                                    <p className="text-[16px] ml-1">Male</p>
+                                    <p className="font-bold 570:text-xl 500:text-md text-[13px] mb-2">Gender</p>
+                                    <p className="570:text-[16px] 500:text-[14px] text-[12px] ml-1">Male</p>
                                 </div>
                                 <div>
-                                    <p className="font-bold text-xl mb-2 overflow-hidden">E-mail</p>
-                                    <p className="text-[16px] ml-1">hajjikarim530@gmail.com</p>
+                                    <p className="font-bold 570:text-xl 500:text-md text-[13px] mb-2 overflow-hidden">E-mail</p>
+                                    <p className="570:text-[16px] 500:text-[14px] text-[12px] ml-1">hajjikarim530@gmail.com</p>
                                 </div>
                             </div>
                             <div className=" w-1/2 flex flex-col  700:items-center items-end">
                             <div>
                                 <div className="mb-[30px]">
-                                    <p className="font-bold text-xl mb-2">Date of Birth</p>
-                                    <p className="text-[16px] ml-1">2001/04/20</p>
+                                    <p className="font-bold 570:text-xl 500:text-md text-[13px] mb-2">Date of Birth</p>
+                                    <p className="570:text-[16px] 500:text-[14px] text-[12px] ml-1">2001/04/20</p>
                                 </div>
                                 <div className="mb-[30px]">
-                                    <p className="font-bold text-xl mb-2">Phone</p>
-                                    <p className="text-[16px] ml-1">0617591321</p>
+                                    <p className="font-bold 570:text-xl 500:text-md text-[13px] mb-2">Phone</p>
+                                    <p className="570:text-[16px] 500:text-[14px] text-[12px] ml-1">0617591321</p>
                                 </div>
                                 <div>
-                                    <p className="font-bold text-xl mb-2">Location</p>
-                                    <p className="text-[16px] ml-1">Tangier</p>
+                                    <p className="font-bold 570:text-xl 500:text-md text-[13px] mb-2">Location</p>
+                                    <p className="570:text-[16px] 500:text-[14px] text-[12px] ml-1">Tangier</p>
                                 </div>
                             </div>
                                 
