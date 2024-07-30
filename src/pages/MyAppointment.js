141f0f4fdce3743ea5e68d0fab5fecb2bc08df45
abdelkarimhajji <abdelkarim.hujji@gmail.com>
2024-07-30 04:09:11 +0100

    i add new card

--- src/pages/MyAppointment.js
+++ src/pages/MyAppointment.js
@@ -1,6 +1,13 @@
 import {useEffect} from "react";
 import NavBar from "../components/NavBar";
 import BottomMenu from "../components/BottomMenu";
+import beard from "../imgs/beard.jpg";
+import beard3 from '../imgs/barber2.avif';
+import beard2 from '../imgs/images.jpeg';
+import { FaHeart } from "react-icons/fa";
+import { MdRateReview } from "react-icons/md";
+import { IoIosPersonAdd } from "react-icons/io";
+import styles from '../styles/App.module.css';
 function MyAppointment()
 {
     return (
@@ -13,9 +20,27 @@ function MyAppointment()
                     <div className="w-1/2 flex items-center justify-center bg-customBlueHover  cursor-pointer" style={{borderColor: "#2d3135"}}>current</div>
                     <div className="w-1/2 flex items-center justify-center cursor-pointer">past</div>
                 </div>
-                <div className="px-[70px] w-full flex gap-x-10">
-                    <div className="w-full h-[250px] bg-customBlue shadow-xl rounded-xl mb-5">d</div>
-                    <div className="w-full h-[250px] bg-customBlue shadow-xl rounded-xl">d</div>
+                <div className=" mb-10 w-full 400:grid grid-cols-2  md:grid-cols-3  gap-x-4 480:gap-x-2 px-5   400:gap-y-3">
+                    <div className="  w-[100%] 400:w-[100%]  sm:h-[320px] h-[320px] shadow-black2 rounded-[20px]  mx-auto cursor-pointer  flex flex-col items-center mb-[20px] 400:mb-0">
+                        <div className="w-full h-[45%] rounded-t-[20px] relative z-0 overflow-hidden">
+                            <img src={beard3} alt=""  className="w-full h-full object-cover rounded-t-[20px]"/>
+                            {/* <div className='absolute top-5 left-4 rounded-xl py-[0.6px] px-[7px] bg-green-700 text-[12px] text-white font-bold flex items-center justify-center'>Open</div> */}
+                            <div className='absolute top-5 left-4 rounded-xl w-[55px] h-[20px] bg-red-800 text-[12px] text-white font-bold flex items-center justify-center'>Closed</div>
+                        </div>
+                        <div className=' w-full text-white pt-4 px-4'>
+                            <p className={`font-bold ${styles.truncate}`}>Salon Dodo</p>
+                            <p className={`text-gray text-sm ${styles.truncate}`}>abdelkarim hajji</p>
+                        </div>
+                        <div className={`w-full  px-4 pt-4 flex  overflow-x-scroll  mb-6 ${styles.hideScrollbar}`}>
+                            <p className='flex items-center text-white flex-shrink-0 '><FaHeart className='text-red-700 480:text-[15px]'/> <span className='ml-1 text-[12px]'>20,200</span></p>
+                            <p className={`text-white ml-6  flex items-center flex-shrink-0 `}><IoIosPersonAdd className='text-green-600  480:text-[15px]'/><span className='text-[12px] ml-1'>1,500</span></p>
+                            <p className={`text-white ml-6  flex items-center flex-shrink-0`}><MdRateReview className='text-green-600 480:text-[15px]'/><span className='text-[12px] ml-1'>1,500</span></p>
+                        </div>  
+                        <div className='w-[100%]   flex justify-center items-end'>
+                            <button type="button" className='text-white text-sm bg-green-700 py-2 mb-3 overflow-hidden sm:px-10 w-[80%] sm:w-auto rounded-xl'>2024/08/20 : 24:45</button>
+                        </div>
+                    </div>
+                
                 </div>
                 <BottomMenu location="posts"/>
                 <div className='mb-[120px]'></div>
